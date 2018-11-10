56143e355dd450005c6043f11745e92fc5817a30
Jack Kolb <jkolb@berkeley.edu>
2018-11-10 15:34:44 -0800

    Fix logical operator precedence, extra quotes in string literals

--- src/main/scala/edu/berkeley/cs/rise/bcdsl/StateMachineParser.scala
+++ src/main/scala/edu/berkeley/cs/rise/bcdsl/StateMachineParser.scala
@@ -3,28 +3,34 @@ package edu.berkeley.cs.rise.bcdsl
 import scala.util.parsing.combinator.JavaTokenParsers
 
 object StateMachineParser extends JavaTokenParsers {
+  def stripQuotes(s: String): String = if (s.startsWith("\"") && s.endsWith("\"")) {
+    s.substring(1, s.length - 1)
+  } else {
+    s
+  }
+
   def dataTypeDecl: Parser[DataType] = "Identity" ^^^ Identity |
-      "Int" ^^^ Int |
-      "String" ^^^ String |
-      "Timestamp" ^^^ Timestamp |
-      "Timespan" ^^^ Timespan |
-      "Bool" ^^^ Bool
+    "Int" ^^^ Int |
+    "String" ^^^ String |
+    "Timestamp" ^^^ Timestamp |
+    "Timespan" ^^^ Timespan |
+    "Bool" ^^^ Bool
 
   def fieldDecl: Parser[Field] = ident ~ ":" ~ dataTypeDecl ^^ { case name ~ ":" ~ ty => Field(name, ty) }
 
   def fieldList: Parser[Seq[Field]] = "data" ~ "{" ~> rep(fieldDecl) <~ "}"
 
   def valueDecl: Parser[SimpleValue] = "now" ^^^ Now |
-      "sender" ^^^ Sender |
-      "true" ^^^ BoolConst(true) |
-      "false" ^^^ BoolConst(false) |
-      "seconds" ^^^ Second |
-      "minutes" ^^^ Minute |
-      "hours" ^^^ Hour |
-      "days" ^^^ Day |
-      wholeNumber ^^  { s => IntConst(s.toInt) } |
-      stringLiteral ^^ StringLiteral |
-      ident ^^ FieldRef
+    "sender" ^^^ Sender |
+    "true" ^^^ BoolConst(true) |
+    "false" ^^^ BoolConst(false) |
+    "seconds" ^^^ Second |
+    "minutes" ^^^ Minute |
+    "hours" ^^^ Hour |
+    "days" ^^^ Day |
+    wholeNumber ^^ { s => IntConst(s.toInt) } |
+    stringLiteral ^^ { s => StringLiteral(stripQuotes(s)) } |
+    ident ^^ FieldRef
 
   def multDiv: Parser[ArithmeticOperator] = "*" ^^^ Multiply | "/" ^^^ Divide
 
@@ -37,44 +43,40 @@ object StateMachineParser extends JavaTokenParsers {
     "<" ^^^ LessThan |
     "<=" ^^^ LessThanOrEqual |
     ">" ^^^ GreaterThan |
-    ">=" ^^^  GreaterThanOrEqual
+    ">=" ^^^ GreaterThanOrEqual
 
   def factor: Parser[Expression] = valueDecl ^^ ValueExpression | "(" ~> arithmeticExpression <~ ")"
 
   def term: Parser[Expression] = chainl1(factor, multDiv ^^
-      (op => (left: Expression, right: Expression) => ArithmeticExpression(left, op, right)))
+    (op => (left: Expression, right: Expression) => ArithmeticExpression(left, op, right)))
 
   def arithmeticExpression: Parser[Expression] = chainl1(term, plusMinus ^^
-      (op => (left: Expression, right: Expression) => ArithmeticExpression(left, op, right)))
+    (op => (left: Expression, right: Expression) => ArithmeticExpression(left, op, right)))
 
   def atom: Parser[Expression] = arithmeticExpression | "(" ~> logicalExpression <~ ")"
 
-  def clause: Parser[Expression] = chainl1(atom, booleanOp ^^
-      (op => (left: Expression, right: Expression) => LogicalExpression(left, op, right)))
+  def clause: Parser[Expression] = chainl1(atom, comparator ^^
+    (op => (left: Expression, right: Expression) => LogicalExpression(left, op, right)))
 
-  def logicalExpression: Parser[Expression] = chainl1(clause, comparator ^^
-      (op => (left: Expression, right: Expression) => LogicalExpression(left, op, right)))
+  def logicalExpression: Parser[Expression] = chainl1(clause, booleanOp ^^
+    (op => (left: Expression, right: Expression) => LogicalExpression(left, op, right)))
 
   def authClause: Parser[AuthDecl] = ident ^^ AuthValue | "(" ~> authExpression <~ ")"
 
   def authExpression: Parser[AuthDecl] = chainl1(authClause, booleanOp ^^
-      (op => (left: AuthDecl, right: AuthDecl) => AuthCombination(left, op, right)))
+    (op => (left: AuthDecl, right: AuthDecl) => AuthCombination(left, op, right)))
 
-  def stateChange: Parser[(Option[String], String)] = opt(ident) ~ "->" ~ ident ^^
-      { case origin ~ "->" ~ destination => (origin, destination) }
+  def stateChange: Parser[(Option[String], String)] = opt(ident) ~ "->" ~ ident ^^ { case origin ~ "->" ~ destination => (origin, destination) }
 
   def authAnnotation: Parser[AuthDecl] = "authorized" ~ "[" ~> authExpression <~ "]"
 
   def guardAnnotation: Parser[Expression] = "requires" ~ "[" ~> logicalExpression <~ "]"
 
-  def assignment: Parser[Assignment] = ident ~ "=" ~ (arithmeticExpression | logicalExpression) ^^
-      { case name ~ "=" ~ expr => Assignment(name, expr) }
+  def assignment: Parser[Assignment] = ident ~ "=" ~ (arithmeticExpression | logicalExpression) ^^ { case name ~ "=" ~ expr => Assignment(name, expr) }
 
   def transitionBody: Parser[Seq[Assignment]] = "{" ~> rep(assignment) <~ "}"
 
-  def transition: Parser[Transition] = stateChange ~ opt(authAnnotation) ~ opt(guardAnnotation) ~ opt(transitionBody) ^^
-      { case (origin, destination) ~ auth ~ guard ~ body => Transition(origin, destination, auth, guard, body) }
+  def transition: Parser[Transition] = stateChange ~ opt(authAnnotation) ~ opt(guardAnnotation) ~ opt(transitionBody) ^^ { case (origin, destination) ~ auth ~ guard ~ body => Transition(origin, destination, auth, guard, body) }
 
-  def stateMachine: Parser[StateMachine] = fieldList ~ rep(transition) ^^
-      { case fields ~ transitions => StateMachine(fields, transitions) }
+  def stateMachine: Parser[StateMachine] = fieldList ~ rep(transition) ^^ { case fields ~ transitions => StateMachine(fields, transitions) }
 }
