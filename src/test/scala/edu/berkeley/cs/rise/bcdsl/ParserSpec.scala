ca7e973394b7352757560f5c1e629cccbbb392a2
Jack Kolb <jkolb@berkeley.edu>
2020-02-29 16:22:38 -0800

    Fixed issue with type checking on timestamp arithmetic, improved unit test organization

--- src/test/scala/edu/berkeley/cs/rise/bcdsl/ParserSpec.scala
+++ src/test/scala/edu/berkeley/cs/rise/bcdsl/ParserSpec.scala
@@ -8,18 +8,6 @@ import org.scalatest.FunSuite
 import scala.io.Source
 
 object ParserSpec {
-  private val TEST_FILES: Seq[String] = Seq(
-    "minimal.txt",
-    "equipment.txt",
-    "auction.txt",
-    "auction2.txt",
-    "auction3.txt",
-    "simpleMultiSig.txt",
-    "majorityMultiSig.txt",
-    "strictMultiSig.txt",
-    "struct.txt",
-  )
-
   private val OUTPUT_DIR = "testOutput"
 }
 
@@ -37,26 +25,26 @@ class ParserSpec extends FunSuite {
     }
   }
 
-  test("Parsing all test files") {
-    new File(ParserSpec.OUTPUT_DIR).mkdir()
-    ParserSpec.TEST_FILES.foreach { fileName =>
-      val spec = parseFile(fileName)
-      writeFile(spec.name + ".spec", spec.toString)
+  def testSpecification(spec: Specification, errorExpected: Boolean = false): Unit = {
+    writeFile(spec.name + ".spec", spec.toString)
 
-      spec.validate() match {
-        case None =>
-          val plusCal = PlusCal.writeSpecification(spec)
-          val generatedTla = TLA.translatePlusCal(spec.name, plusCal)
+    spec.validate() match {
+      case None =>
+        val plusCal = PlusCal.writeSpecification(spec)
+        val generatedTla = TLA.translatePlusCal(spec.name, plusCal)
 
-          writeFile(s"${spec.name}.tla", TLA.modifyGeneratedTLA(generatedTla))
-          writeFile(s"${spec.name}MC.tla", TLA.writeSpecificationToAux(spec))
-          writeFile(s"${spec.name}MC.cfg", TLA.writeSpecificationToConfig(spec))
+        writeFile(s"${spec.name}.tla", TLA.modifyGeneratedTLA(generatedTla))
+        writeFile(s"${spec.name}MC.tla", TLA.writeSpecificationToAux(spec))
+        writeFile(s"${spec.name}MC.cfg", TLA.writeSpecificationToConfig(spec))
 
-          writeFile(spec.name + ".sol", Solidity.writeSpecification(spec))
+        writeFile(spec.name + ".sol", Solidity.writeSpecification(spec))
 
-        case Some(err) =>
-          println(s"Error in $fileName: $err")
-      }
+      case Some(err) =>
+        if (!errorExpected) {
+          fail(err)
+        } else {
+          println(s"Error in ${spec.name}: $err")
+        }
     }
   }
 
@@ -64,5 +52,50 @@ class ParserSpec extends FunSuite {
     val writer = new PrintWriter(s"${ParserSpec.OUTPUT_DIR}/$fileName", StandardCharsets.UTF_8)
     writer.println(contents)
     writer.close()
+  }
+
+  test("Mimimal contract with type error") {
+    val spec = parseFile("minimal.txt")
+    testSpecification(spec, errorExpected = true)
+  }
+
+  test("Simple equipment management contract") {
+    val spec = parseFile("equipment.txt")
+    testSpecification(spec)
+  }
+
+  test("Auction version 1") {
+    val spec = parseFile("auction.txt")
+    testSpecification(spec)
+  }
+
+  test("Auction version 2") {
+    val spec = parseFile("auction2.txt")
+    testSpecification(spec)
+  }
+
+  test("Auction version 3") {
+    val spec = parseFile("auction3.txt")
+    testSpecification(spec)
+  }
+
+  test("Simple multi-signature wallet") {
+    val spec = parseFile("simpleMultiSig.txt")
+    testSpecification(spec)
+  }
+
+  test("Strict multi-signature wallet") {
+    val spec = parseFile("strictMultiSig.txt")
+    testSpecification(spec)
+  }
+
+  test("Contract with simple structs") {
+    val spec = parseFile("struct.txt")
+    testSpecification(spec, errorExpected = true)
+  }
+
+  test("Simple timestamp arithmetic") {
+    val spec = parseFile("timestampMath.txt")
+    testSpecification(spec)
   }
 }
