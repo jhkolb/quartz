4efbf8983361993184e1f68bdaa97c5734e293d1
John Yang <john.yang20@berkeley.edu>
2020-11-06 09:23:25 -0800

    Single loop 1st draft

--- src/main/scala/edu/berkeley/cs/rise/quartz/PayableExtractor.scala
+++ src/main/scala/edu/berkeley/cs/rise/quartz/PayableExtractor.scala
@@ -2,105 +2,49 @@ package edu.berkeley.cs.rise.quartz
 
 object PayableExtractor {
 
-  private val BUILTIN_PARAMS: Set[String] = Set("tokens")
-
-  private var fields: Set[String] = Set.empty[String]
-  private var params: Set[(String, String)] = Set.empty[(String, String)]
-  private var structFields: Set[(String, String)] = Set.empty[(String, String)]
-
   def extractPayableVars(stateMachine: StateMachine): (Set[String], Set[(String, String)], Set[(String, String)]) = {
-    // Determine Payable Fields w/ Fix Point Iteration
-    var previousSize = fields.size
+    var fields: Set[String] = Set.empty[String]
+    var params: Set[(String, String)] = Set.empty[(String, String)]
+    val structFields: Set[(String, String)] = Set.empty[(String, String)]
+
+    var previousSize = 0
     do {
-      previousSize = fields.size
-      extractPayableFields(flattenStatements(stateMachine), stateMachine.fields.map(_.name).toSet)
-    } while (previousSize != fields.size)
+      previousSize = fields.size + params.size + structFields.size
+      stateMachine.transitions foreach { transition =>
+        val transParams = transition.parameters.getOrElse(Seq.empty[Variable]).map(row => row.name)
+        transition.body.getOrElse(Seq.empty[Statement]) foreach {
+          case Send(destination, _, _) => {
+            val destVar = identifyVariable(destination)
+            if (transParams.contains(destVar)) {
+              params += ((transition.name, destVar))
+            } else {
+              fields += destVar
+            }
+          }
+          case Assignment(left, right) if fields.contains(left.rootName) ||
+            params.contains((transition.name, left.rootName)) => {
+            val rightVar = identifyVariable(right)
+            if (transParams.contains(rightVar)) {
+              params += ((transition.name, rightVar))
+            } else {
+              fields += rightVar
+            }
+          }
+          case _ => "default"
+        }
+      }
+    } while (previousSize != fields.size + params.size + structFields.size)
 
-//    println("FIELDS: " + fields)
-
-    // Determine Payable Params w/ Fix Point Iteration
-    stateMachine.transitions foreach { transition => extractPayableParams(transition) }
-
-//    println("PARAMS: " + params)
-
-    val structMap = stateMachine.fields.filter(p => p.ty.isInstanceOf[Struct])
-      .map(variable => variable.name -> variable.ty).toMap
-    previousSize = structFields.size
-    do {
-      previousSize = structFields.size
-      extractPayableStruct(flattenStatements(stateMachine), structMap)
-    } while (previousSize != structFields.size)
-
-    println("STRUCTS: " + structFields)
-
+    println("Fields:", fields)
+    println("Params:", params)
+    println("Struct:", structFields)
     return (fields, params, structFields)
   }
 
-  private def extractPayableStruct(statements: Seq[Statement], structMap: Map[String, DataType]): Unit = {
-    structFields = statements.foldLeft(structFields) { (current, statement) =>
-      println(statement)
-      statement match {
-        case Send(destination, _, _) if destination.isInstanceOf[StructAccess] =>
-          current.union(Set((getStructType(destination, structMap), destination.asInstanceOf[StructAccess].field)))
-        case _ => current
-      }
-    }
-  }
-
-  private def getStructType(expression: Expression, structMap: Map[String, DataType]): String = expression match {
-    // TODO: How to figure out data type of field for first case?
-    // TODO: How to handle Mappings, Sequences
-    case StructAccess(struct, _) if struct.isInstanceOf[StructAccess] => {
-//      val varName = struct.asInstanceOf[StructAccess].field
-//      var rootStruct =
-//      while (struct.isInstanceOf[StructAccess]) {
-//        struct = struct.asInstanceOf[StructAccess].struct
-//      }
-//      println(struct.rootName)
-      struct.asInstanceOf[StructAccess].field
-    }
-    case StructAccess(struct, _) if struct.isInstanceOf[VarRef] =>
-      struct.determinedType.toString.stripPrefix("Wrapper(").stripSuffix(")")
-    case _ => ""
-  }
-
-  private def extractPayableParams(transition: Transition) = {
-    var previousSize = params.size
-    do {
-      previousSize = params.size
-      params = transition.body.getOrElse(Seq.empty[Statement]).foldLeft(params) { (current, statement) =>
-        statement match {
-          case Send(destination, _, _) => current.union(Set((transition.name, extractHelper(destination))))
-          case Assignment(left, right) if current.contains((transition.name, left.rootName)) || fields.contains(left.rootName) => current.union(Set((transition.name, extractHelper(right))))
-          case _ => current
-        }
-      }
-    } while (params.size != previousSize)
-  }
-
-  private def extractPayableFields(statements: Seq[Statement], scope: Set[String]) = {
-    val names = statements.foldLeft(fields) { (current, statement) =>
-      statement match {
-        case Send(destination, _, _) => current.union(Set(extractHelper(destination)))
-        case Assignment(left, right) if current.contains(left.rootName) => current.union(Set(extractHelper(right)))
-        case _ => current
-      }
-    }
-    if (scope.nonEmpty) {
-      fields = names.intersect(scope)
-    } else {
-      fields = names
-    }
-  }
-
-  private def extractHelper(expression: Expression): String = expression match {
-    case MappingRef(map, key) => extractHelper(map)
+  def identifyVariable(expression: Expression): String = expression match {
+    case MappingRef(map, key) => identifyVariable(map)
     case VarRef(name) => name
-    case LogicalOperation(left, _, right) => extractHelper(left) ++ extractHelper(right)
-    case ArithmeticOperation(left, _, right) => extractHelper(left) ++ extractHelper(right)
-    case SequenceSize(sequence) => extractHelper(sequence)
+    case SequenceSize(sequence) => identifyVariable(sequence)
     case _ => ""
   }
-
-  private def flattenStatements(stateMachine: StateMachine): Seq[Statement] = stateMachine.transitions.flatMap(_.body.getOrElse(Seq.empty[Statement]))
 }
